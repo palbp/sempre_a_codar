0916fc39a53c8bdfd3568f7c9f29175f0448565d
Paulo Pereira <palbp.giga@gmail.com>
2021-02-17 12:07:39 +0000

    Cleans up and documents code associated to the displaying of the game timer in the drawing screen.

--- Series/Building_DRAG_forAndroid/project/app/src/main/java/edu/sempreacodar/drag/draw/DrawingActivity.kt
+++ Series/Building_DRAG_forAndroid/project/app/src/main/java/edu/sempreacodar/drag/draw/DrawingActivity.kt
@@ -1,6 +1,8 @@
 package edu.sempreacodar.drag.draw
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.sempreacodar.drag.databinding.ActivityDrawingBinding
@@ -28,22 +30,54 @@ class DrawingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val timerValue = getTimerValueFromIntent()
-        binding.word.text = getWordFromIntent()
-        binding.timer.text = timerValue.toString()
+        viewModel.timerValue.observe(this) { renderUI() }
+        viewModel.state.observe(this) { renderUI() }
 
-        viewModel.maybeStartTimer(timerValue)
-        viewModel.timerValue.observe(this) {
-            binding.timer.text = it.toString()
-        }
+        viewModel.maybeStartTimer(getTimerValueFromIntent())
 
         // TODO: Collect user input and update drawing accordingly via ViewModel
     }
 
+    /**
+     * Renders the screen according to its current state. The UI state is fully specified to ensure
+     * that the implementation is not dependent of any state transitions; it only depends on the
+     * current state.
+     */
+    private fun renderUI() {
+        binding.word.text = getWordFromIntent()
+        binding.timer.text = viewModel.timerValue.value.toString()
+
+        val timerValue = viewModel.timerValue.value
+        fun isTimeRunningOut(): Boolean = viewModel.state.value == DrawingViewModel.State.DRAWING
+                && timerValue != null
+                && timerValue.toSeconds() < 10
+
+        val timerColor = if (isTimeRunningOut()) theme.getColor(android.R.attr.textColor)
+        else theme.getColor(android.R.attr.textColorHighlight)
+        binding.timer.setTextColor(timerColor)
+
+        // TODO: render according to the screen's state
+    }
+
+    /**
+     * Helper method used to get the word to be drawn from the received intent
+     */
     private fun getWordFromIntent() =
         if (intent.hasExtra(WORD_EXTRA)) intent.getStringExtra(WORD_EXTRA)
         else "Pintainho"
 
+    /**
+     * Helper method used to get the initial timer value from the received intent
+     */
     private fun getTimerValueFromIntent() =
             GameTimer.fromSeconds(intent.getIntExtra(TIMER_VALUE_EXTRA, 60))
+}
+
+/**
+ * Helper extension function used to get the color with the given attribute id from the theme.
+ */
+private fun Resources.Theme.getColor(attrId: Int): Int {
+    val typedValue = TypedValue()
+    resolveAttribute(attrId, typedValue, true)
+    return typedValue.data
 }
--- Series/Building_DRAG_forAndroid/project/app/src/main/java/edu/sempreacodar/drag/draw/DrawingViewModel.kt
+++ Series/Building_DRAG_forAndroid/project/app/src/main/java/edu/sempreacodar/drag/draw/DrawingViewModel.kt
@@ -6,31 +6,62 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import edu.sempreacodar.drag.draw.model.GameTimer
 import edu.sempreacodar.drag.draw.model.currentTimeSeconds
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-suspend fun countDownFrom(value: GameTimer, publishTo: MutableLiveData<GameTimer>) {
-        val startTimeInSeconds = currentTimeSeconds()
-        val endTimeInSeconds = startTimeInSeconds + value.toSeconds()
+/**
+ * Suspending function that delays execution for the specified [time] while publishing the remaining
+ * time to [publishTo]. The function returns once the specified time elapses.
+ */
+suspend fun countDownFrom(time: GameTimer, publishTo: MutableLiveData<GameTimer>) {
+    val startTimeInSeconds = currentTimeSeconds()
+    val endTimeInSeconds = startTimeInSeconds + time.toSeconds()
 
-        while(currentTimeSeconds() < endTimeInSeconds) {
-            delay(250)
-            publishTo.value = GameTimer.fromSeconds((endTimeInSeconds - currentTimeSeconds()).toInt())
-        }
+    while(currentTimeSeconds() < endTimeInSeconds) {
+        delay(250)
+        publishTo.value = GameTimer.fromSeconds((endTimeInSeconds - currentTimeSeconds()).toInt())
+    }
 }
 
+/**
+ * The view model for the drawing screen.
+ */
 class DrawingViewModel : ViewModel() {
 
-    private val _timerValue = MutableLiveData<GameTimer>()
-    val timerValue: LiveData<GameTimer> = _timerValue
+    enum class State {
+        INITIALIZED, DRAWING, TIME_EXPIRED
+    }
 
-    val model = Drawing()
+    /**
+     * The current timer value
+     */
+    val timerValue: LiveData<GameTimer> = MutableLiveData()
+    private val _timerValue = timerValue as MutableLiveData<GameTimer>
 
-    fun maybeStartTimer(theTimerValue: GameTimer) {
+    /**
+     * The current screen state
+     */
+    val state: LiveData<State> = MutableLiveData(State.INITIALIZED)
+    private val _state = state as MutableLiveData<State>
 
-        // TODO: make this conditional. Only start if not started
-        _timerValue.value = theTimerValue
-        viewModelScope.launch { countDownFrom(theTimerValue, _timerValue) }
+    /**
+     * Starts the game timer if the screen is in it's initial state
+     * @param theTimerValue the timer's initial value
+     * @return the [Job] instance corresponding to the count down timer that was started, or null
+     * if the timer was not started
+     */
+    fun maybeStartTimer(theTimerValue: GameTimer): Job? {
+
+        if (state.value != State.INITIALIZED)
+            return null
+
+        return viewModelScope.launch {
+            _state.value = State.DRAWING
+            _timerValue.value = theTimerValue
+            countDownFrom(theTimerValue, _timerValue)
+            _state.value = State.TIME_EXPIRED
+        }
     }
 
     // TODO: Extend view model with drawing operations
