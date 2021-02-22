3fc9490bbabe5317e724a9fa6cd38a6a6e23c568
Paulo Pereira <palbp.giga@gmail.com>
2021-02-22 15:30:48 +0000

    Adds skeleton for first Compose Activity. It also tunes the required dependencies.

---
+++ Series/Building_DRAG_forAndroid/project/app/src/main/java/edu/sempreacodar/drag/draw/NewDrawingActivity.kt
@@ -0,0 +1,38 @@
+package edu.sempreacodar.drag.draw
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import edu.sempreacodar.drag.draw.ui.theme.DRAGTheme
+
+class NewDrawingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DRAGTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DRAGTheme {
+        Greeting("Android")
+    }
+}
